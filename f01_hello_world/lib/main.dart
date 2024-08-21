f463b8b598b36bc2d371e56c2de74c543fe5fc1e
jeanmmlima <jeanmariomlima@gmail.com>
2024-08-21 15:41:08 -0300

    main only

--- f01_hello_world/lib/main.dart
+++ f01_hello_world/lib/main.dart
@@ -1,1 +1,12 @@
-
+import 'package:flutter/material.dart';
+void main() {
+  runApp(
+    const Center(
+      child: Text(
+        "Olá mundo!",
+        textDirection: TextDirection.ltr,
+        style: TextStyle(fontSize: 30),
+      ),
+    ),
+  );
+}
