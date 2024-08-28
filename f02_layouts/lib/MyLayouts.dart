a990614b721f7947a4c860bb6081b6d64358d343
jeanmmlima <jeanmariomlima@gmail.com>
2024-08-28 15:23:34 -0300

    expanded e ajustes

--- f02_layouts/lib/MyLayouts.dart
+++ f02_layouts/lib/MyLayouts.dart
@@ -7,28 +7,51 @@ class MyLayouts extends StatelessWidget {
   Widget build(BuildContext context) {
 
     const double boxSize = 50;
+    /*1. mudar o main AxisAlgiment
+    2. mudar o tamanho para ver que usa o espaço necessário
+    3. aumentar o numero de caixas para dar overflow
+    4. Usar expanded
+    5. Aplicar outra linha
+    6. Variar props na coluna
+    */
 
     return Scaffold(
       appBar: AppBar(title: Text("Meus Layouts"),),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget> [
-            Container(
-              padding: EdgeInsets.symmetric(vertical: boxSize, horizontal: boxSize),
-              color: Colors.red,
-              child: Text("1"),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget> [
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: boxSize),
+                    color: Colors.red,
+                    child: Text("1",textAlign: TextAlign.center),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: boxSize),
+                    color: Colors.blue,
+                    child: Text("1",textAlign: TextAlign.center),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: boxSize),
+                    color: Colors.green,
+                    child: Text("1", textAlign: TextAlign.center,),
+                  ),
+                ),
+                
+              ],
             ),
-            Container(
-              padding: EdgeInsets.symmetric(vertical: boxSize, horizontal: boxSize),
-              color: Colors.blue,
-              child: Text("1"),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(vertical: boxSize, horizontal: boxSize),
-              color: Colors.green,
-              child: Text("1"),
-            ),
+
           ],
         ),
       ),
