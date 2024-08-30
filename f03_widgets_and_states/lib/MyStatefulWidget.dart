794b1c765c3146fdc4b54863d84fc899394b19c8
jeanmmlima <jeanmariomlima@gmail.com>
2024-08-30 15:46:56 -0300

    contador ok

--- f03_widgets_and_states/lib/MyStatefulWidget.dart
+++ f03_widgets_and_states/lib/MyStatefulWidget.dart
@@ -8,28 +8,66 @@ class MyStatefulWdget extends StatefulWidget {
 }
 
 class _MyStatefulWdgetState extends State<MyStatefulWdget> {
+  int _contador = 0;
 
-  int contador = 0;
+  void _contar() {
+    setState(() {
+      _contador++;
+    });
+
+    print(_contador);
+  }
+
+  void _zerar() {
+    setState(() {
+      _contador = 0;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Meu Widget Stateful"),
-        backgroundColor: Colors.purple,
-      ),
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            children: <Widget>[
-              Text("O valor do contador é: $contador"),
-              SizedBox(height: 16,),
-              ElevatedButton(onPressed: (){}, child: Text("Contar"),),
-            ],
-          ),
+        appBar: AppBar(
+          title: Text("Meu Widget Stateful"),
+          backgroundColor: Colors.purple,
         ),
-      )
-    );
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "O valor do contador é: ",
+                  style: TextStyle(fontSize: 20),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  "$_contador",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                ElevatedButton(
+                  onPressed: _contar,
+                  child: Text(
+                    "Contar",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                TextButton(
+                  onPressed: _zerar,
+                  child: Text(
+                    "Zerar",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
--- f03_widgets_and_states/lib/main.dart
+++ f03_widgets_and_states/lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:f03_widgets_and_states/MyStatefulWidget.dart';
 import 'package:f03_widgets_and_states/MyStatelessWidget.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: MyStatelessWidget(text: "Meu componente stateless",),
+    home: MyStatefulWdget(),
   ),);
 }
 
