50fe074f8ab0a8c4824b475bc7a4f76653bd12a2
jeanmmlima <jeanmariomlima@gmail.com>
2024-08-30 14:25:26 -0300

    base para o 2 exemplo

---
+++ f02_layouts/lib/MyLayouts2.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class MyLayouts2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Exemplo de Layout', style: TextStyle(color: Colors.white),),
+          backgroundColor: Colors.pink,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                padding: EdgeInsets.all(16.0),
+                color: Colors.blue,
+                child: Text(
+                  'Container no topo (Column)',
+                  style: TextStyle(color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(height: 16.0), // Espaçamento entre containers
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(16.0),
+                      color: Colors.red,
+                      child: Text(
+                        'Container à esquerda (Row)',
+                        style: TextStyle(color: Colors.white),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 16.0), // Espaçamento entre os containers na Row
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(16.0),
+                      color: Colors.green,
+                      child: Text(
+                        'Container à direita (Row)',
+                        style: TextStyle(color: Colors.white),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16.0), // Espaçamento entre containers
+              Container(
+                padding: EdgeInsets.all(16.0),
+                color: Colors.orange,
+                child: Text(
+                  'Container na base (Column)',
+                  style: TextStyle(color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+        ),
+    );
+  }
+}
--- f02_layouts/lib/main.dart
+++ f02_layouts/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:f02_layouts/MyLayouts.dart';
+import 'package:f02_layouts/MyLayouts2.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: const MyLayouts(),
+      home: MyLayouts2(),
     ),
   );
 }
